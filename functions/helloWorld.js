4ce885262c428a06e4d88f64687055ad076338be
Christopher Collins <chris@ccollins.io>
2020-07-09 23:01:41 -0600

    updated for async function

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,8 +1,19 @@
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
   return {
+    statusCode: 200,
+    body: JSON.stringify({
+      msg: "Hello World",
+    }),
+  };
+};
+
+/* 
+exports.handler = (event, context, callback) => {
+  callback(null, {
     statusCode: 200,
     body: JSON.stringify({
       msg: "Hello World!",
     }),
-  };
-};
+  });
+}; 
+*/
