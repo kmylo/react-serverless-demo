1b350538d95d8d71621aa2a4557a444eeb250b95
Christopher Collins <chris@ccollins.io>
2020-07-10 00:25:55 -0600

    created function to saveHighScores

--- functions/saveHighScore.js
+++ functions/saveHighScore.js
@@ -9,8 +9,6 @@ const base = Airtable.base(process.env.AIRTABLE_BASE);
 const table = base.table(process.env.AIRTABLE_TABLE);
 
 exports.handler = async (event) => {
-  console.log(event);
-
   if (event.httpMethod !== "POST") {
     return {
       statusCode: 405,
@@ -18,27 +16,60 @@ exports.handler = async (event) => {
     };
   }
 
+  const { score, name } = JSON.parse(event.body);
+
+  if (typeof score === "undefined" || !name) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ err: "Bad request" }),
+    };
+  }
+
+  console.log(score);
+
   try {
     const records = await table
       .select({
         sort: [{ field: "score", direction: "desc" }],
-        filterByFormula: `AND(name != "", score > 0)`,
+        // filterByFormula: `AND(name != "", score > 0)`,
       })
       .firstPage();
 
-    const formatedRecords = records.map((record) => ({
+    const formattedRecords = records.map((record) => ({
       id: record.id,
       fields: record.fields,
     }));
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(formatedRecords),
-    };
+    console.log(formattedRecords);
+    const lowestRecord = formattedRecords[formattedRecords.length - 1];
+    console.log(lowestRecord);
+
+    if (
+      typeof lowestRecord.fields.score === "undefined" ||
+      score > lowestRecord.fields.score
+    ) {
+      const updatedRecord = {
+        id: lowestRecord.id,
+        fields: { name, score },
+      };
+
+      console.log("updatedRecord", updatedRecord);
+
+      await table.update([updatedRecord]);
+      return {
+        statusCode: 200,
+        body: JSON.stringify(formattedRecords),
+      };
+    } else {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({}),
+      };
+    }
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ err: "Failed to query records in Airtable." }),
+      body: JSON.stringify({ err: "Failed to save score in Airtable." }),
     };
   }
 };
